4d99d4811b3715b13d39f1139991747f5d37c12b
Kamalakannan <kamalakkannan_rajamanickam@trimble.com>
2019-03-23 11:01:18 +0530

    jashd

--- src/com/test/merge/BaseClass.java
+++ src/com/test/merge/BaseClass.java
@@ -3,20 +3,23 @@ package com.test.merge;
 public class BaseClass {
 
 	public static void main(String[] args) {
-		System.out.println("Updated the Baseclass with somechanges");
+		System.out.println("I Not assfigafuibgdfgadgfgdhkcgdhk");
 
 	}
 	
 	public void selectDropdown() {
-		
+		System.out.println("I Not assfigafuibgdfgadgfgdhkcgdhk");
+
 	}
 
 	
 	public void selectCheckbox() {
-		
+		System.out.println("I Not assfigafuibgdfgadgfgdhkcgdhk");
+
 	}
 	
 	public void launchBrowser() {
-		
+		System.out.println("I Not assfigafuibgdfgadgfgdhkcgdhk");
+
 	}
 }
